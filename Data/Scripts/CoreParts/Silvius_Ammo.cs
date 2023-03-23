95ebfb27f60ed82ac9dd07a80b9384249d92e0a4
Muzz <dalymitchell11@gmail.com>
2023-03-23 15:51:04 -0600

    nerfed impulse on monopole to litterally nothing

--- Data/Scripts/CoreParts/Silvius_Ammo.cs
+++ Data/Scripts/CoreParts/Silvius_Ammo.cs
@@ -35,6 +35,7 @@ namespace Scripts
             AmmoMagazine = "Energy", // SubtypeId of physical ammo magazine. Use "Energy" for weapons without physical ammo.
             AmmoRound = "Silvius Gauss", // Name of ammo in terminal, should be different for each ammo type used by the same weapon. Is used by Shrapnel.
             HybridRound = true, // Use both a physical ammo magazine and energy per shot.
+            //draws 231mw
             EnergyCost = 0.1f, // Scaler for energy per shot (EnergyCost * BaseDamage * (RateOfFire / 3600) * BarrelsPerShot * TrajectilesPerBarrel). Uses EffectStrength instead of BaseDamage if EWAR.
             BaseDamage = 47500f, // Direct damage; one steel plate is worth 100.
             Mass = 0f, // In kilograms; how much force the impact will apply to the target.
