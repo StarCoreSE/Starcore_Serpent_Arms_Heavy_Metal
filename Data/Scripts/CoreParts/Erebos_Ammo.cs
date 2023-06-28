0b46115fbb3554f9e3e92091e64aab389290a4a4
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-06-28 17:47:40 -0500

    actually its only the second stage that clips through in the grid

--- Data/Scripts/CoreParts/Erebos_Ammo.cs
+++ Data/Scripts/CoreParts/Erebos_Ammo.cs
@@ -127,7 +127,7 @@ namespace Scripts
                     Armor = -1f, // Multiplier for damage against all armor. This is multiplied with the specific armor type multiplier (light, heavy).
                     Light = -1f, // Multiplier for damage against light armor.
                     Heavy = -1f, // Multiplier for damage against heavy armor.
-                    NonArmor = 0.01f, // Multiplier for damage against every else.
+                    NonArmor = -1f, // Multiplier for damage against every else.
                 },
                 Shields = new ShieldDef
                 {
